66179fc31ec3f4549a1ae32cf53427b496286a66
mu1664al-s <mu1664al-s@student.lu.se>
2019-03-01 17:00:34 +0100

    conflict solved + modules for angular/cli 7.3.4

--- AdaptationFinder/src/app/app.module.ts
+++ AdaptationFinder/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 
 import { HttpClientModule }    from '@angular/common/http';
 
-=======
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
->>>>>>> 68282e1f54b867997a17380432cbbb5aa85dad1a
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatDividerModule} from '@angular/material';
@@ -14,7 +11,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatgridComponent } from './matgrid/matgrid.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import {MatButtonModule} from '@angular/material/button'; 
-import {MatInputModule} from '@angular/material/input'; 
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -25,15 +22,12 @@ import {MatInputModule} from '@angular/material/input';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    HttpClientModule
-=======
+    HttpClientModule,
     BrowserAnimationsModule,
     MatDividerModule,
     MatGridListModule,
     MatButtonModule,
     MatInputModule
->>>>>>> 68282e1f54b867997a17380432cbbb5aa85dad1a
   ],
   providers: [],
   bootstrap: [AppComponent]
