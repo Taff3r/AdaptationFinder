bcee67162cbfa6f295a06273890850393feccd7a
draganadzaip <dat14dad@student.lu.se>
2019-03-01 14:51:47 +0100

    added component for gridlist

--- AdaptationFinder/src/app/app.module.ts
+++ AdaptationFinder/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatDividerModule} from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatgridComponent } from './matgrid/matgrid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatgridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
