808600fec3434b67b03193e908995138c7bdc532
mu1664al-s <mu1664al-s@student.lu.se>
2019-03-04 16:59:44 +0100

    har gjort en hel del med remote-data service. kvar att göra: hämta individuel film info och bok covers

--- AdaptationFinder/src/app/remote-data.service.ts
+++ AdaptationFinder/src/app/remote-data.service.ts
@@ -10,23 +10,64 @@ export class RemoteDataService {
 
   constructor(private http: HttpClient,  private resultsService: ResultsService) { }
 
+  search(param: any): void {
+    // TODO: send the message _after_ fetching the heroes
+    //this.messageService.addData(this.fetchMovies("the lord of the rings"));
+    //this.messageService.addData(this.fetchBooks("the lord of the rings"));
+    this.resultsService.addData(this.fetchMovies(param.movie));
+    this.resultsService.addData(this.fetchBooks(param.book));
+  }
+
   private remoteServiceInit() {
     return this.http.post("https://openlibrary.org/account/login", '{"username": "ol0273st-s@student.lu.se", "password": "1234"}');
   }
 
-  //$ curl -i -H 'Content-Type: application/json' -d '{"username": "ol0273st-s@student.lu.se", "password": "1234"}' https://openlibrary.org/account/login
-
-  private fetch(url: string) {
+  private fetch(url: string): any {
     return this.http.get(url);
   }
 
-  fetchMovies(key: string) {
-    const url = "http://www.omdbapi.com/?apikey=f22abc29&t=" + key;
-    fetch(url).then(response => response.json()).then(object => this.resultsService.addData(object));
+  fetchMovies(key: string): any {
+    const url = "http://www.omdbapi.com/?apikey=f22abc29&s=" + this.filter(key);
+    return fetch(url).then(response => response.json()).then(result => this.filterData(["Title", "Year", "Type", "Poster", "imdbID"], result.Search, "").filter(media => media.Type !== "game"));
   }
 
-  fetchBooks(key: string) {
-    const url = "http://openlibrary.org/search.json?title=" + key.toLowerCase().split(" ").reduce((newKey, keyPart) => (newKey += "+" + keyPart), "");
-    fetch(url).then(response => response.json()).then(object => this.resultsService.addData(object));
+  fetchBooks(key: string): any {
+    const url = "http://openlibrary.org/search.json?title=" + this.filter(key);
+    return fetch(url).then(response => response.json()).then(result => this.filterData(["title", "author_name", "isbn"], result.docs, "isbn"));
   }
+
+  private filterData(keys: any[], data: any[], noCompare: string): any {
+    let temp = data.map(entry => keys.map(key => ({[key]: entry[key]})).reduce((result, data) => ({...result, ...data}), {}));
+    return this.uniqueEntries(temp, noCompare);
+  }
+
+  private uniqueEntries(data: any[], noCompare: string): any[] {
+    let temp = [];
+    if (data.length > 1) {
+      temp = [...this.uniqueEntries(data.filter(next => !this.compareObjects(next, data[0], noCompare)), noCompare), data[0]];
+    } else {
+      temp = [...data];
+    }
+    return temp;
+  }
+
+  private compareObjects(obj1: any, obj2: any, noCompare: string): boolean {
+    const stringCheck = Object.keys(obj1).filter(key => (typeof obj1[key] === "string" && key !== noCompare)).map(key => (obj1[key].toLowerCase() === obj2[key].toLowerCase())).reduce((result, check) => {result = result && check; return result}, true);
+    const arrayCheck = Object.keys(obj1).filter(key => (obj1[key] instanceof Array && key !== noCompare)).map(key => this.compareArray(obj1[key], obj2[key])).reduce((result, check) => {result = result && check; return result}, true);
+    return stringCheck && arrayCheck;
+  }
+
+  private compareArray(array1: string[], array2: string[]): boolean {
+    if (array1 !== undefined && array2 !== undefined) {
+      const intersection = array1.filter(key1 => array2.find(key2 => key2.toLowerCase() === key1.toLowerCase()) !== undefined).length;
+      return intersection === array1.length && intersection === array2.length;
+    }
+    return false;
+  }
+
+  private filter(value: string): string {
+    const tempValue = value.toLowerCase().split(" ").reduce((newKey, keyPart) => (newKey += keyPart + "+"), "");
+    return tempValue.slice(0, tempValue.length - 1)
+  }
+
 }
