8ddb647813b8d3c17e1743de5b8bf4e9f98ddc21
simon <simon.tenggren@gmail.com>
2019-03-05 15:25:18 +0100

    server now allows cors, and some more progress on searchbar

--- AdaptationFinder/src/server.js
+++ AdaptationFinder/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const mariaDB = require('mariadb');
 // Connection Object, used to reach the database server.
 var conn;
