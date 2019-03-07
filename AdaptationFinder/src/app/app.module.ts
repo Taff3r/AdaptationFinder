b2b31fe6bee7d8a46f08cc2838722519a2f57669
simon <simon.tenggren@gmail.com>
2019-03-07 11:31:16 +0100

    forgot app.module

--- AdaptationFinder/src/app/app.module.ts
+++ AdaptationFinder/src/app/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
-import {MatTableModule} from '@angular/material/table';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatDividerModule} from '@angular/material';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import {MatButtonModule} from '@angular/material/button'; 
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatInputModule } from '@angular/material/input';
 import { ListviewComponent } from './listview/listview.component';
 import { CouplerComponent, CouplerInputComponent} from './coupler/coupler.component'; 
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import { BoovietableComponent } from './boovietable/boovietable.component';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatRadioModule} from '@angular/material/radio'; 
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatRadioModule } from '@angular/material/radio'; 
+import { MovietableComponent } from './movietable/movietable.component';
+import { BooktableComponent } from './booktable/booktable.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,8 @@ import {MatRadioModule} from '@angular/material/radio';
     ListviewComponent,
     CouplerComponent,
     CouplerInputComponent,
-    BoovietableComponent
+    MovietableComponent,
+    BooktableComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +42,7 @@ import {MatRadioModule} from '@angular/material/radio';
     FormsModule,
     MatTableModule,
     MatCardModule,
-    MatRadioModule
+    MatRadioModule,
   ],
   entryComponents:[
     CouplerInputComponent,
