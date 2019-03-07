7f76cb371fab60129a6a8af9fe4d5a9f89b98613
simon <simon.tenggren@gmail.com>
2019-03-07 18:43:30 +0100

    formatted this abomination some would call code

--- AdaptationFinder/src/app/coupler/coupler.component.ts
+++ AdaptationFinder/src/app/coupler/coupler.component.ts
@@ -23,35 +23,35 @@ export class CouplerComponent implements OnInit {
   styleUrls: ['./couplerInput.component.css']
 })
 export class CouplerInputComponent{
-books:any[]=[];
-movies:any[]=[];
-movieSelection;
-bookSelection;
+   books:any[]=[];
+   movies:any[]=[];
+   movieSelection;
+   bookSelection;
   constructor(private bottomSheetRef: MatBottomSheetRef<CouplerInputComponent>, private rds:RemoteDataService, private db:DatabaseService){}
   openLink(event:MouseEvent): void{
     this.bottomSheetRef.dismiss(),
     event.preventDefault();
-	  }
+  }
    bookSubmit(event:any){
-	  console.log("test");
-	  this.rds.fetchBooks(event.target.value).then(results =>  {this.books = results; document.getElementById("bookSearch").click()});
-	  }
-	  movieSubmit(event:any){
-	  this.rds.fetchMovies(event.target.value).then(results => {this.movies = results; document.getElementById("movieSearch").click()});
-	  }
-	  databaseSubmit(event:any){
-	  console.log("works?");
-	  if(!(this.movieSelection && this.bookSelection)){ alert("please make BOTH selections");
-	  }
-	  else{
-	  this.db.addConnection(this.movieSelection,this.bookSelection);
-    this.bottomSheetRef.dismiss();
-	  }
-	  }
-	  changeMovie(event:any){
-          this.movieSelection = event.value;
-	  }
-	  changeBook(event:any){
-	  this.bookSelection = event.value;
-	  }
+      console.log("test");
+      this.rds.fetchBooks(event.target.value).then(results =>  {this.books = results; document.getElementById("bookSearch").click()});
+   }
+  movieSubmit(event:any){
+     this.rds.fetchMovies(event.target.value).then(results => {this.movies = results; document.getElementById("movieSearch").click()});
+  }
+  databaseSubmit(event:any){
+     console.log("works?");
+     if(!(this.movieSelection && this.bookSelection)){
+        alert("please make BOTH selections");
+     }else{
+        this.db.addConnection(this.movieSelection,this.bookSelection);
+        this.bottomSheetRef.dismiss();
+     }
+  }
+  changeMovie(event:any){
+     this.movieSelection = event.value;
+  }
+  changeBook(event:any){
+     this.bookSelection = event.value;
+  }
 }
