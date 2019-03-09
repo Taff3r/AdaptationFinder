d2ef7b2ec64a73c95df68b8c99a015e2a0e22713
simon <simon.tenggren@gmail.com>
2019-03-09 16:10:48 +0100

    added connections now shown both ways

--- AdaptationFinder/src/app/booktable/booktable.component.ts
+++ AdaptationFinder/src/app/booktable/booktable.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { DialogWindowComponent } from '../dialog-window/dialog-window.component';
 import { RemoteDataService }  from '../remote-data.service';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-booktable',
@@ -14,20 +15,21 @@ export class BooktableComponent implements OnInit {
   @Input() books : any ; 
   displayedColumns: string[] = ['title', 'author_name', 'isbn'];
 
-  constructor(private dialog: MatDialog, private rds: RemoteDataService) { }
+  constructor(private dialog: MatDialog, private rds: RemoteDataService, private dbs: DatabaseService) { }
 
   ngOnInit() {
   }
 
   openDialog(row): void {
-    if(row.isbn){
-      this.rds.fetchBook(row.isbn).then(r => {
-        let data = {title: r.title, poster: r.cover, maker: r.author_name};
-        console.log(data);
-        this.dialog.open(DialogWindowComponent, { data: {title: r.title, poster: r.cover, maker: "Author: " + r.author_name }});
-    });
+   if(row.isbn) {
+      this.dbs.fecthBookConnections(row.isbn).then(result =>
+        Promise.all(result.map(movie => this.rds.fetchMovie(movie.imdbID))))
+      .then(
+        result => this.rds.fetchBook(row.isbn).then(r => {
+          let fixed = [];
+          result.forEach(o => fixed.push({...o, title: o["Title"]}));
+          this.dialog.open(DialogWindowComponent, { data: {title: r.title, poster: r.cover, maker: "Author: " + r.author_name, plot: r.title, conn: fixed}})}));
+    }
   }
-
-}
 }
 
